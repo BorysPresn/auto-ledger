4790cb92f706e38d928fa069ca05bf25c20b96ea
BorysPresn <boryspresniak@gmail.com>
2026-05-23 15:17:56 +0200

    feat: enhance layout and styling for app header and main components

--- src/widgets/app-header/ui/AppHeader.tsx
+++ src/widgets/app-header/ui/AppHeader.tsx
@@ -1,10 +1,13 @@
+import styles from "./AppHeader.module.scss";
 
 export const AppHeader = () => {
   return (
-    <header>
-      <p>LOGO</p>
-      <h1>Auto Ledger</h1>
-      <button type="button">Logout</button>
+    <header className={styles.header}>
+      <span className={styles.logo}>AL</span>
+      <h1 className={styles.title}>Auto Ledger</h1>
+      <button className={styles.logoutButton} type="button">
+        Logout
+      </button>
     </header>
   );
 };
