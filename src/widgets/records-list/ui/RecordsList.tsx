dcc80b200849ce5704345de16c7b06f0fb4929bc
BorysPresn <boryspresniak@gmail.com>
2026-06-23 12:52:07 +0200

    feat: display history records as cards

--- src/widgets/records-list/ui/RecordsList.tsx
+++ src/widgets/records-list/ui/RecordsList.tsx
@@ -1,19 +1,28 @@
 import { FuelRecordDetails, type FuelRecord } from "../../../entities";
+import { Icon } from "../../../shared";
+import styles from "./RecordsList.module.scss";
 
 interface RecordsListProps {
   records: FuelRecord[];
 }
 
 export const RecordsList = ({ records }: RecordsListProps) => {
+  if (records.length === 0) {
+    return (
+      <p className={styles.empty}>
+        <Icon name="tasks" />
+        No fuel records yet
+      </p>
+    );
+  }
+
   return (
-    <ul>
-      {records.length !== 0
-        ? records.map((record) => (
-            <li key={record.createdAt}>
-              <FuelRecordDetails record={record} />
-            </li>
-          ))
-        : "No records yet..."}
+    <ul className={styles.recordsList}>
+      {records.map((record) => (
+        <li key={record.createdAt} className={styles.recordsCard}>
+          <FuelRecordDetails record={record} />
+        </li>
+      ))}
     </ul>
   );
 };
