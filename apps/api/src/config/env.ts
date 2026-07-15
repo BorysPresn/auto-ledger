8f385b1e7d9720f5ddf31b0676fef256cf1b5e87
Borys Presniak <boryspresniak@gmail.com>
2026-07-15 18:55:39 +0200

    chore(api):conaigure MongoDB connection (#10)

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -2,10 +2,19 @@ import { z } from "zod";
 import "dotenv/config";
 
 const envSchema = z.object({
+  DNS_SERVER: z.string().trim().optional(),
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
   PORT: z.coerce.number().int().min(1).max(65535).default(3000),
+  MONGODB_URI: z
+    .string()
+    .trim()
+    .min(1, { message: "MONGO_DB_URI is required" }),
+  MONGODB_NAME: z
+    .string()
+    .trim()
+    .min(1, { message: "MONGODB_DB_NAME is required" }),
 });
 
 const result = envSchema.safeParse(process.env);
